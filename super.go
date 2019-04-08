fc61b6701ba639bf2c206aa84ede0a8f45d93a58
Mark Karpeles <magicaltux@gmail.com>
2019-04-08 10:31:48 +0900

    squash wip

--- super.go
+++ super.go
@@ -3,13 +3,16 @@ package squashfs
 import (
 	"bytes"
 	"encoding/binary"
+	"errors"
 	"io"
+	"log"
 	"reflect"
 )
 
 // https://dr-emann.github.io/squashfs/
 type Superblock struct {
-	fs io.ReaderAt
+	fs    io.ReaderAt
+	order binary.ByteOrder
 
 	Magic             uint32
 	InodeCnt          uint32
@@ -34,17 +37,45 @@ type Superblock struct {
 
 func New(fs io.ReaderAt) (*Superblock, error) {
 	sb := &Superblock{fs: fs}
+	head := make([]byte, sb.binarySize())
+
+	log.Printf("squash: read header %d bytes", len(head))
+	_, err := fs.ReadAt(head, 0)
+	if err != nil {
+		return nil, err
+	}
+	log.Printf("squash: read header, parsing")
+	err = sb.UnmarshalBinary(head)
+	if err != nil {
+		return nil, err
+	}
+
+	return sb, nil
 }
 
 func (s *Superblock) UnmarshalBinary(data []byte) error {
-	v := reflect.ValueOf(s)
+	v := reflect.ValueOf(s).Elem()
 	c := v.NumField()
 	r := bytes.NewReader(data)
+
+	switch string(data[:4]) {
+	case "hsqs":
+		s.order = binary.LittleEndian
+	case "sqsh":
+		s.order = binary.BigEndian
+	default:
+		return errors.New("invalid squashfs partition")
+	}
 
 	// Decode
 	var err error
 	for i := 0; i < c; i++ {
-		err = binary.Read(r, order, v.Field(i).Interface())
+		c := v.Type().Field(i).Name[0]
+		if c < 'A' || c > 'Z' {
+			continue
+		}
+		log.Printf("read %s", v.Type().Field(i).Name)
+		err = binary.Read(r, s.order, v.Field(i).Interface())
 		if err != nil {
 			return err
 		}
@@ -54,12 +85,16 @@ func (s *Superblock) UnmarshalBinary(data []byte) error {
 }
 
 func (s *Superblock) binarySize() int {
-	v := reflect.ValueOf(s)
+	v := reflect.ValueOf(s).Elem()
 	c := v.NumField()
-	s := 0
+	sz := uintptr(0)
 
 	for i := 0; i < c; i++ {
-		s += v.Field(i).Type().Size()
+		c := v.Type().Field(i).Name[0]
+		if c < 'A' || c > 'Z' {
+			continue
+		}
+		sz += v.Field(i).Type().Size()
 	}
-	return s
+	return int(sz)
 }
